86f9c9f5fba18ce163bd64c167e9d19496d87714
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-04-28 15:44:23 +0200

    Uncontrolled form

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+import QuestionForm from './QuestionForm'
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+      <QuestionForm />
 			{/* <Cart /> */}
 			<ShoppingList />
 		</div>
