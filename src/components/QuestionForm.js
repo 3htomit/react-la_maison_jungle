51036e4ebeb1438c74a14a6eed285ad0452e69fd
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-04-28 17:48:32 +0200

    Controlled form trial

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,15 +1,25 @@
-function handleSubmit(e) {
-  e.preventDefault()
-  alert(e.target['my_input'].value)
-}
+import { useState } from 'react'
 
 function QuestionForm() {
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type='text' name='my_input' defaultValue='Tapez votre texte' />
-      <button type='submit'>Entrer</button>
-    </form>
-  )
+    const [inputValue, setInputValue] = useState('Posez votre question ici')
+    function checkValue(value) {
+      if (!value.includes('f')) {
+          setInputValue(value)
+      }
+    }
+    return (
+      <div>
+        <div>
+          <textarea
+            value={inputValue}
+            onChange={(e) => checkValue(e.target.value)}
+            />
+          <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+        </div>
+        {/* {inputError(inputValue)} */}
+      </div>
+    )
 }
+
 
 export default QuestionForm
