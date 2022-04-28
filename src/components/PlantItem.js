d77c1230f4d4aecafad9a7edbb2ae99af6c5531b
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-04-28 15:06:48 +0200

    OnClik event added to plant items

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -6,9 +6,13 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function handleClick(plantName) {
+  alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
 function PlantItem({name, cover, id, best, light, water}) {
   return (
-    <li key={id} class='lmj-plant-item'>
+    <li key={id} class='lmj-plant-item' onClick={() => handleClick(name)}>
       <PlantTags best={best} />
       <img src={cover} alt='Plant' className='lmj-plant-item-cover' />
       {capitalize(name)}
