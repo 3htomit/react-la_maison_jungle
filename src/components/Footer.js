399e5f1513a944f5a37252469553eb56db34e8e6
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-05-02 12:13:06 +0200

    Foot email input onBlur validation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,35 @@
 import { useState } from 'react'
 import '../styles/Footer.css'
 
+function checkEmail(value) {
+  // console.log(!value.includes('@'))
+  if (!value.includes('@')) {
+    alert("⚠️ Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.");
+  }
+}
+
 function Footer() {
-	const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('')
 
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
-				Pour les passionné·e·s de plantes 🌿🌱🌵
+				<p>Pour les passionné·e·s de plantes</p>
+        <p>🌿🌱🌵</p>
 			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<div className='lmj-footer-elem'>
+        Laissez-nous votre mail :
+        <form>
+          <input
+            type='text'
+            name='my_input'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onBlur={(e) => checkEmail(e.target.value)}
+            ></input>
+          <button>Enregistrer</button>
+        </form>
+      </div>
 		</footer>
 	)
 }
